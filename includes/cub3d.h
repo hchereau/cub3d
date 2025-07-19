b3d7036bebc7cfb630d888019cbb4987b848f9c0
Chereau hugo <hchereau42@gmail.com>
2025-07-19 10:03:57 +0200

    add parsing structures

---
+++ includes/cub3d.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 09:40:59 by linux             #+#    #+#             */
+/*   Updated: 2025/07/19 09:51:11 by linux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+typedef int		t_color;
+typedef char	t_direction;
+typedef char **	t_grid;
+
+typedef struct s_coord
+{
+	int	x;
+	int	y;
+}	t_coord;
+
+typedef struct s_map
+{
+	t_grid	grid;
+	int		width;
+	int		height;
+}	t_map;
+
+typedef struct s_texture
+{
+	char	*north;
+	char	*south;
+	char	*east;
+	char	*west;
+}	t_texture;
+
+typedef struct s_player
+{
+	t_coord		pos;
+	t_direction	dir;
+}	t_player;
+
+typedef struct s_config
+{
+	t_texture	textures;
+	t_color		floor_color;
+	t_color		ceiling_color;
+	t_map		map;
+	t_player	player;
+}	t_config;
+
+
+#endif
