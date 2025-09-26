8a7f3f57cea6e4eb2bbcff2f6313affcb421cc14
Hugo Chereau <hchereau42@gmail.com>
2025-09-26 20:31:05 +0200

    test

--- srcs/parse/parse_utils.c
+++ srcs/parse/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:07:53 by linux             #+#    #+#             */
-/*   Updated: 2025/09/15 16:15:03 by linux            ###   ########.fr       */
+/*   Updated: 2025/09/26 20:30:20 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,24 @@ int	is_empty_line(char *line)
 
 int	is_map_line(char *line)
 {
-	while (*line == ' ')
-		line++;
-	return (*line == '1' || *line == '0' || *line == 'N'
-		|| *line == 'S' || *line == 'E' || *line == 'W');
+	size_t	i;
+
+	if (!line)
+		return (0);
+	i = 0;
+	while (line[i] == ' ' || line[i] == '\t')
+		i++;
+	if (line[i] == '\0')
+		return (0);
+	while (line[i])
+	{
+		if (line[i] != ' ' && line[i] != '\t' && line[i] != '0'
+			&& line[i] != '1' && line[i] != 'N' && line[i] != 'S'
+			&& line[i] != 'E' && line[i] != 'W')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 void	free_split(char **tab)
