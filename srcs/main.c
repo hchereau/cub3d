b66315f3916022f061ee17295e3704f0fc93244f
Hugo Chereau <hchereau42@gmail.com>
2025-09-30 14:04:50 +0200

    v1

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 20:16:40 by linux             #+#    #+#             */
-/*   Updated: 2025/09/29 20:57:50 by linux            ###   ########.fr       */
+/*   Updated: 2025/09/30 11:47:33 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv)
 	ft_memset(&cfg, 0, sizeof(cfg));
 	parse_file(argv[1], &cfg);
 	printf("Loaded map %dx%d\n", cfg.map.width, cfg.map.height);
-	(void)exec_game(cfg);
+	exec_game(cfg);
 	free_config(&cfg);
 	return (EXIT_SUCCESS);
 }
